4f06249a06bcedb099d4cf1c3460879a9ec6e223
GauriSasikumar <gaurisasikumar04@gmail.com>
2025-02-09 20:42:30 +0530

    collective signup and login page

--- Gauri Frontend/src/App.jsx
+++ Gauri Frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import Dashboard from "./components/dashboard/dashboard.jsx";
+import LoginRegister from "./components/login-signup/login.jsx";
+import SignupLogin from "./components/login-signup/signup.jsx"; 
 
-function App() {
-  return <Dashboard />;
+function App(){
+  return(
+    <div>
+      <SignupLogin/>
+      
+    </div>
+  );
 }
+
 
 export default App;
 
