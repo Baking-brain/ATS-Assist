5e70b4fc9e83163eced6fe5b7023b190e9bb7f4a
GauriSasikumar <gaurisasikumar04@gmail.com>
2025-03-17 21:52:49 +0530

    created the "Profile Creation" component, both jsx and css

--- Gauri Frontend/src/App.jsx
+++ Gauri Frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import Dashboard from "./components/dashboard/dashboard.jsx";
 import Login from "./components/login-signup/login.jsx";
 import SignupLogin from "./components/login-signup/signup.jsx"; 
-
+import CreateProfile from "./components/createprofile.jsx";
 function App(){
   return(
     <div>
-      <Login/>
+      <CreateProfile/>
       
     </div>
   );
